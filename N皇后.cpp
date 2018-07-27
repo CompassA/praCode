d0d0f546dcdf997a4fe287b5cf25b528954e322c
CompassA <31817512+CompassA@users.noreply.github.com>
2018-07-27 22:16:43 +0800

    Add files via upload

---
+++ N皇后.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+void generate(vector<int> &P, vector<int> &occur, const int &index, const int &n, int &count) {
+	if (index == n) {
+		++count;
+		return;
+	}
+	
+	for (int i = 1; i <= n; ++i) {
+		if (occur[i] == 0) {
+			bool isOK = true;
+			for (int j = 0; j < index; ++j) {
+				if (abs(j - index) == abs(P[j] - i)) {
+					isOK = false;
+					break;
+				}
+			}
+			
+			if (isOK) {
+				occur[i] = 1;
+				P.push_back(i);
+				generate(P, occur, index+1, n, count);
+				occur[i] = 0;
+				P.pop_back(); 
+			}
+		}
+	}
+}
+
+int main() {
+	for (int i = 8; i <= 17; ++i) {
+		vector<int> P, occur(20, 0);
+		int count = 0;
+		generate(P, occur, 0, i, count);
+		
+		printf("%d»Êºó£º%d\n", i, count);
+	}
+	return 0;
+} 
